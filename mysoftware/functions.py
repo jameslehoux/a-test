99c9a96a8ace92bc4a3defb42769c9a5a0601241
JLeHoux <jplh1g11@soton.ac.uk>
2018-10-31 11:16:36 +0000

    Added root finding functions

--- mysoftware/functions.py
+++ mysoftware/functions.py
@@ -1,6 +1,28 @@
 import numpy as np
 
 def square(x):
+    """
+    Takes a number x and squares it.
+
+    Parameters:
+    -----------
+    x, float or int:
+        Number which is to be squared.
+
+    Returns:
+    --------
+    float:
+        Square of x
+
+    Examples:
+    ---------
+    >>> square(2)
+    4
+
+    >>> square(-1)
+    1
+
+    """
     return x*x
 
 def coulomb(r):
@@ -12,6 +34,29 @@ def CentralDifference(f, x, h):
    #         2*h
    return (f(x + h) - f(x - h))/(2*h)
 
-for i in range(2,8):
-   h = 10**(-i)
-   print(h, abs(CentralDifference(np.sin, 0.0, h) - 1.0))
+def myfunc(x):
+    return (x**2 + (x - 2)**3 - 5)
+
+def mysinfunc(x):
+    return (np.sin(x**2) - x + 5)
+
+def mysquaredfunc(x):
+    return (x - 2)**2
+
+def mytanfunc(x):
+    return np.tan(x)
+
+def mybisection(func, a, b, eps):
+    fa = func(a)
+    fb = func(b)
+    if fa * fb > 0:
+        print('No root between these bounds')
+        return None
+    while abs(b - a) > eps:
+        x = 0.5 * (a + b)
+        fx = func(x)
+        if fa * fx < 0:
+            b = x
+        else:
+            a = x
+    return x
